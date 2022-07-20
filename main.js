69e568d8fdcd71a6228a832bafe8b1d3e821b48b
Suji Park <lzns960@gmail.com>
2022-07-20 16:36:02 +0900

    add

---
+++ main.js
@@ -0,0 +1,25 @@
+// SUBMENU > SEARCH
+const searchEl = document.querySelector(".search");
+const searchInputEl =  searchEl.querySelector("input");
+
+searchEl.addEventListener("click", function(){
+    searchInputEl.focus();
+})
+searchInputEl.addEventListener("focus", function(){
+    searchInputEl.setAttribute("placeholder", "통합 검색");
+})
+
+//포커스의 반대되는 이벤트 blur
+searchInputEl.addEventListener("blur", function(){
+    searchInputEl.setAttribute("placeholder", "");
+})
+
+//SWIPER
+//NOTICE >> SWIPER
+const swiperNotice = new Swiper("notice .inner .inner__left .swiper", {
+    direction: "vertical", //문자열 형식으로 줘야 한다.
+    loop: true,
+    autoplay: true,
+});
+
+console.log(swiperNotice);
